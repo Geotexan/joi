91302bfe46f3fdd4e9e4d912a9e2efe0ee2ef7dd
Francisco José Rodríguez Bogado <bogado@qinn.es>
2020-08-05 15:23:15 +0200

    Beta

--- conversationbot.py
+++ conversationbot.py
@@ -15,6 +15,11 @@ bot.
 """
 
 import logging
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
@@ -39,6 +44,9 @@ def start(update, context):
         'Escribe /cancelar para parar en cualquier momento.\n'
         '(Ojo a la barra «/» al introducir comandos)'
         '\n\n'
+        'Si no recibes respuesta, es posible que tenga algún problema (todavía'
+        ' estoy en pruebas). Usa el correo electrónico entonces.'
+        '\n\n'
         'Antes de empezar, asegúrate de que has reiniciado el ordenador o '
         'has cerrado sesión **por completo** y vuelto a hacer __login__ si eres '
         'usuario de escritorio remoto. Si el problema es con Murano, '
@@ -61,27 +69,32 @@ def start(update, context):
 
 def gravedad(update, context):
     user = update.message.from_user
-    logger.info("Incidencia de %s. Gravedad: %s", user.first_name, update.message.text)
+    gravedad = update.message.text
+    logger.info("Incidencia de %s. Gravedad: %s", user.first_name, gravedad)
     update.message.reply_text('Ya veo. ¿Puedes enviarme una captura de '
                               'pantalla o una foto del error?\n'
                               'Escribe /saltar si quieres saltarte este paso.'
                               '\n\n'
                               '(Si adjuntas una imagen, marca «enviar como foto»)',
                               reply_markup=ReplyKeyboardRemove())
-
+    context.user_data['gravedad'] = gravedad
     return CAPTURA
 
 
 def captura(update, context):
     user = update.message.from_user
     photo_file = update.message.photo[-1].get_file()
-    photo_file.download('user_photo.jpg')
-    logger.info("Captura de %s: %s", user.first_name, 'user_photo.jpg')
+    fotoname = 'user_photo_{}_{}.jpg'.format(user.first_name,
+                                             datetime.datetime.now().strftime("%Y%m%d_%H:%M:%S"))
+    photo_file.download(fotoname)
+    captura = fotoname
+    logger.info("Captura de %s: %s", user.first_name, fotoname)
     update.message.reply_text('¡Genial! Ahora descríbeme el problema con todos'
                               'los detalles que puedas o escribe /saltar.'
                               'Cuantos más detalles proporciones, más rápido '
                               'podré ayudarte.')
 
+    context.user_data['captura'] = captura
     return DESCRIPCION
 
 
@@ -99,11 +112,16 @@ def skip_captura(update, context):
 
 def descripcion(update, context):
     user = update.message.from_user
+    descripcion = update.message.text
     logger.info("Descripción del problema de %s: %s", user.first_name,
-                update.message.text)
+                descripcion)
     update.message.reply_text('¡Gracias! Me podré en contacto contigo lo antes'
                               ' posible.')
-
+    context.user_data['descripcion'] = descripcion
+    asunto = "{}: Incidencia de {}".format(user_data['gravedad'], user.full_name)
+    cuerpo = context.user_data['descripcion']
+    adjunto = context.user_data['captura']
+    send_mail(asunto, cuerpo, adjunto)
     return ConversationHandler.END
 
 
@@ -153,6 +171,53 @@ def main():
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
+
+
+def send_mail(asunto, texto, adjunto):
+    """Envía un correo con el asunto, texto y adjunto recibidos desde la
+    cuenta de informática a la cuenta de incidencias."""
+    rte = config.gmail_user
+    dest = config.gmail_dest
+    passwd = config.gmail_password
+    # https://www.geeksforgeeks.org/send-mail-attachment-gmail-account-using-python/
+    fromaddr = rte
+    toaddr = dest
+    # instance of MIMEMultipart
+    msg = MIMEMultipart()
+    # storing the senders email address
+    msg['From'] = fromaddr
+    # storing the receivers email address
+    msg['To'] = toaddr
+    # storing the subject
+    msg['Subject'] = asunto
+    # string to store the body of the mail
+    body = texto
+    # attach the body with the msg instance
+    msg.attach(MIMEText(body, 'plain'))
+    # open the file to be sent
+    filename = adjunto
+    attachment = open(adjunto, "rb")
+    # instance of MIMEBase and named as p
+    p = MIMEBase('application', 'octet-stream')
+    # To change the payload into encoded form
+    p.set_payload((attachment).read())
+    # encode into base64
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    # attach the instance 'p' to instance 'msg'
+    msg.attach(p)
+    # creates SMTP session
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    # start TLS for security
+    s.starttls()
+    # Authentication
+    s.login(fromaddr, passwd)
+    # Converts the Multipart msg into a string
+    text = msg.as_string()
+    # sending the mail
+    s.sendmail(fromaddr, toaddr, text)
+    # terminating the session
+    s.quit()
 
 
 if __name__ == '__main__':
